00192bef590e05329b61e36208abd2231308de1c
Antoni Kirylczuk <antekkirylczuk@gmail.com>
2025-03-06 15:59:46 +0100

    Dodanie klasy FizzBuzz

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ namespace FizzBuzzApp
     {
         private int zakres;
 
-        // Konstruktor przyjmujący zakres liczb
+        
         public FizzBuzz(int maxLiczba)
         {
             zakres = maxLiczba;
         }
 
-        // Metoda do wyświetlania FizzBuzz
+       
         public void WyswietlFizzBuzz()
         {
             for (int i = 1; i <= zakres; i++)
@@ -36,8 +36,8 @@ namespace FizzBuzzApp
             Console.Write("Podaj górny zakres liczb: ");
             if (int.TryParse(Console.ReadLine(), out int liczba))
             {
-                FizzBuzz fizzBuzz = new FizzBuzz(liczba); // Tworzymy obiekt z podanym zakresem
-                fizzBuzz.WyswietlFizzBuzz(); // Wywołujemy metodę
+                FizzBuzz fizzBuzz = new FizzBuzz(liczba); 
+                fizzBuzz.WyswietlFizzBuzz(); 
             }
             else
             {
